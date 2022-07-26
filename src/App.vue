43e17db222c4a280ef90433f01a44c848ce167c1
igordmax <igordmax@gmail.com>
2022-07-26 13:15:13 +0300

    firat_step

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<header> verh verh verh verh verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh  verh </header>
+ <div class="container">
+  <div class="flex left_side_menu cd-accordion-menu">  
+<TreeComponent :model="treeData"></TreeComponent>
+</div>
+<div class="flex main_section">
+основная секция основная секция основная секция основная секция
+</div>
+<div class="flex right_side_menu">
+pravo pravo pravo 
+pravo pravo pravo
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import TreeComponent from './components/TreeComponent.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TreeComponent 
+  },
+    data: function() {
+    return {
+      treeData: treeData
+    }
+  }, 
+
+};
+let treeData= {
+
+        name: "group",        
+  children: [
+    {
+      name: "Sub Group",
+      children: [{ name: "Item" }, { name: "Item" }]
+    },
+     {
+      name: "Sub Group1",
+      children: [
+        { name: "Item1" }, 
+        { name: "Item1", 
+         children: [
+           {name: "subsub"}
+         ]
+        }
+      ]
+     },
+    { name: "Item" }
+  ] 
+                };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+display: -ms-flexbox;
+display: flex;
 }
+.flex{
+  position: relative;
+    width: 100%;
+}
+.left_side_menu {
+  -ms-flex: 1 0 0;
+  flex: 1 0 0;
+  min-width: 200px;
+  max-width: 500px;
+}
+.main_section{
+-ms-flex: 1 0 0;
+    flex: 1 0 0;
+}
+.right_side_menu {
+  -ms-flex: 1 0 0;
+  flex: 1 0 0;
+  min-width: 200px;
+  max-width: 500px;
+}
+
 </style>
---
+++ src/components/TreeComponent.vue
@@ -0,0 +1,149 @@
+<template>
+<li :class="[isFolder ? 'folder' : 'file']">
+ 
+   <label id=clickToAct @click="makeActive" 
+   >
+          <button >
+      <span :class="{'open': open}"
+      @click="toggle">
+      </span>
+     </button>
+          
+      {{ model.name }} 
+
+    </label>
+    <ul v-show="open" v-if="isFolder" :class="{'open': open}">
+      <TreeComponent
+        v-for="(model, index) in model.children"
+        :key="index"
+        :model="model">
+      </TreeComponent>
+    </ul>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'TreeComponent',
+  props: {
+    model: Object
+  },
+  data: function() {
+    return {
+      open: true
+    };
+  },
+  computed: {
+    isFolder: function(){
+     return this.model.children && this.model.children.length;
+    }
+  },
+  methods: {
+    stopClick: function (e) {
+      e.preventDefault();
+    },
+    toggle: function () {
+      if (this.isFolder) {
+        this.open= !this.open;
+      }
+    }
+  },
+ 
+
+};
+
+</script>
+
+
+<style>
+.cd-accordion-menu {
+padding: 0px 0px 4px;
+position: relative;
+left: 0%;
+right: 79.5%;
+top: 0.15%;
+bottom: 0%;
+/*Light/Background/Fill Color/Solid Background/Base */
+background: #F3F3F3;
+}
+
+ol, ul, li {
+	list-style: none;
+  padding: 0px;
+}
+
+.cd-accordion-menu li {
+  user-select: none;
+  
+}
+
+.cd-accordion-menu label, .cd-accordion-menu a {
+  position: relative;
+  display: block;
+  /*padding: 18px 18px 18px 45px;
+  box-shadow: inset 1px 1px #000;
+  
+  color: rgba(0,0,0,.8);*/
+}
+
+.cd-accordion-menu label::before,
+.cd-accordion-menu label::after,
+.cd-accordion-menu a::after {
+  /* icons */
+content: '';
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.cd-accordion-menu label {
+  cursor: pointer;
+}
+
+.cd-accordion-menu button {     
+    background-color: Transparent;
+    background-repeat:no-repeat;
+    border: none;
+    cursor:pointer;
+    overflow: hidden;       
+}
+
+/*adding icons*/
+.cd-accordion-menu li.folder >label> button span::before {
+  content: "pp";
+  font-family: 'Font Awesome 5 Free', 'Font Awesome 5 Brands';
+  font-weight: 900; 
+}
+.cd-accordion-menu li.folder >label> button span.open::before {
+ 
+   content: "pp";
+  font-family: 'Font Awesome 5 Free', 'Font Awesome 5 Brands';
+  font-weight: 900; 
+}
+
+.cd-accordion-menu ul label:hover{
+  background: rgba(218,235,247,1);
+}
+.cd-accordion-menu ul a:hover {
+  background: rgba(218,235,247,1);
+}
+.cd-accordion-menu ul label:active{  
+
+  background: rgba(0, 0, 0, 0.0373);
+/* Light/Fill Color/Subtle/Secondary */
+
+border: 1px solid rgba(0, 0, 0, 0.0373);
+border-radius: 4px;
+}
+
+.cd-accordion-menu > li:last-of-type > label,
+.cd-accordion-menu > li:last-of-type > a,
+.cd-accordion-menu > li > ul > li:last-of-type label,
+.cd-accordion-menu > li > ul > li:last-of-type a {
+  box-shadow: none;
+}
+
+</style>
